fc50aa58a73151cabdba02d793250b7ed49b1d92
이소라 <rasolee0303@gmail.com>
2023-07-21 21:46:47 +0900

    feat: 추가 baegopa_store_code_data_v0.0.10

---
+++ sql/v0.0.10/baegopa_store_code_data_v0.0.10.sql
@@ -0,0 +1,74 @@
+drop DATABASE if exists baegopa_store_dev;
+create DATABASE baegopa_store_dev;
+use baegopa_store_dev;
+
+INSERT INTO baegopa_store_dev.reward_point_type (reward_point_type_code, name, reward_point) VALUES ('A1', '회원가입', 5000);
+INSERT INTO baegopa_store_dev.reward_point_type (reward_point_type_code, name, reward_point) VALUES ('A2', '일반 리뷰 작성', 500);
+INSERT INTO baegopa_store_dev.reward_point_type (reward_point_type_code, name, reward_point) VALUES ('A3', '사진 리뷰 작성', 1000);
+
+INSERT INTO baegopa_store_dev.notification_type (notification_type_code, name) VALUES ('A1', '두레이');
+INSERT INTO baegopa_store_dev.notification_type (notification_type_code, name) VALUES ('A2', '텔레그램');
+INSERT INTO baegopa_store_dev.notification_type (notification_type_code, name) VALUES ('A3', '카카오톡');
+
+INSERT INTO baegopa_store_dev.user_status (user_status_code, name) VALUES ('A1', '활성');
+INSERT INTO baegopa_store_dev.user_status (user_status_code, name) VALUES ('A2', '휴면');
+INSERT INTO baegopa_store_dev.user_status (user_status_code, name) VALUES ('A3', '탈퇴');
+
+INSERT INTO baegopa_store_dev.anniversary_type (anniversary_type_code, name) VALUES ('A1', '생일');
+INSERT INTO baegopa_store_dev.anniversary_type (anniversary_type_code, name) VALUES ('A2', '애인 생일');
+INSERT INTO baegopa_store_dev.anniversary_type (anniversary_type_code, name) VALUES ('A3', '모 생일');
+INSERT INTO baegopa_store_dev.anniversary_type (anniversary_type_code, name) VALUES ('A4', '부 생일');
+INSERT INTO baegopa_store_dev.anniversary_type (anniversary_type_code, name) VALUES ('A5', '결혼기념일');
+
+INSERT INTO baegopa_store_dev.day (day_code, name) VALUES ('A1', '월');
+INSERT INTO baegopa_store_dev.day (day_code, name) VALUES ('A2', '화');
+INSERT INTO baegopa_store_dev.day (day_code, name) VALUES ('A3', '수');
+INSERT INTO baegopa_store_dev.day (day_code, name) VALUES ('A4', '목');
+INSERT INTO baegopa_store_dev.day (day_code, name) VALUES ('A5', '금');
+INSERT INTO baegopa_store_dev.day (day_code, name) VALUES ('A6', '토');
+INSERT INTO baegopa_store_dev.day (day_code, name) VALUES ('A7', '일');
+
+INSERT INTO baegopa_store_dev.delivery_price (delivery_price_id, price) VALUES (1, 5000);
+
+INSERT INTO baegopa_store_dev.oauth_type (oauth_type_code, name) VALUES ('A1', '페이코');
+INSERT INTO baegopa_store_dev.oauth_type (oauth_type_code, name) VALUES ('A2', '네이버');
+INSERT INTO baegopa_store_dev.oauth_type (oauth_type_code, name) VALUES ('A3', '카카오');
+
+INSERT INTO baegopa_store_dev.order_state (order_state_code, name) VALUES ('A1', '대기');
+INSERT INTO baegopa_store_dev.order_state (order_state_code, name) VALUES ('A2', '조리');
+INSERT INTO baegopa_store_dev.order_state (order_state_code, name) VALUES ('A3', '배달');
+INSERT INTO baegopa_store_dev.order_state (order_state_code, name) VALUES ('A4', '완료');
+INSERT INTO baegopa_store_dev.order_state (order_state_code, name) VALUES ('A5', '취소');
+
+INSERT INTO baegopa_store_dev.pay_status (pay_status_code, name) VALUES ('A1', '결제중');
+INSERT INTO baegopa_store_dev.pay_status (pay_status_code, name) VALUES ('A2', '결제완료');
+INSERT INTO baegopa_store_dev.pay_status (pay_status_code, name) VALUES ('A3', '결제실패');
+
+INSERT INTO baegopa_store_dev.pay_type (pay_type_code, name) VALUES ('A1', '토스');
+INSERT INTO baegopa_store_dev.pay_type (pay_type_code, name) VALUES ('A2', 'PAYCO');
+INSERT INTO baegopa_store_dev.pay_type (pay_type_code, name) VALUES ('A3', '만나서결제');
+INSERT INTO baegopa_store_dev.pay_type (pay_type_code, name) VALUES ('A4', '현금결제');
+INSERT INTO baegopa_store_dev.pay_type (pay_type_code, name) VALUES ('A5', '신용카드');
+
+
+INSERT INTO baegopa_store_dev.store_role (store_role_code, name) VALUES ('A1', '대표');
+INSERT INTO baegopa_store_dev.store_role (store_role_code, name) VALUES ('A2', '매니저');
+INSERT INTO baegopa_store_dev.store_role (store_role_code, name) VALUES ('A3', '알바');
+
+INSERT INTO baegopa_store_dev.user_role(user_role_code, name) VALUES ('A1', '사용자');
+INSERT INTO baegopa_store_dev.user_role(user_role_code, name) VALUES ('A2', '관리자');
+
+INSERT INTO baegopa_store_dev.user_rank (user_rank_code, name) VALUES ('A1', '프렌드');
+INSERT INTO baegopa_store_dev.user_rank (user_rank_code, name) VALUES ('A2', '패밀리');
+INSERT INTO baegopa_store_dev.user_rank (user_rank_code, name) VALUES ('A3', '마스터');
+INSERT INTO baegopa_store_dev.user_rank (user_rank_code, name) VALUES ('A4', 'VIP');
+
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A1', '한식');
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A2', '일식');
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A3', '중식');
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A4', '양식');
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A5', '분식');
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A6', '패스트푸드');
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A7', '아시안');
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A8', '디저트');
+INSERT INTO baegopa_store_dev.store_category (store_category_code, name) VALUES ('A9', '야식');
